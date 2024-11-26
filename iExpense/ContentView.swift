01c64f0dd1bac22082943881ee3abe6973a229b4
Mark Santoro <mark_santoro@comcast.net>
2024-11-26 15:13:05 -0800

    App complete

--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -17,7 +17,17 @@ struct ContentView: View {
         NavigationStack{
             List {
                 ForEach(expenses.items) {item in
-                    Text(item.name)
+                    HStack{
+                        VStack(alignment: .leading){
+                            Text(item.name)
+                                .font(.headline)
+                            Text(item.type)
+                            
+                        }
+                        
+                        Spacer()
+                        Text(item.amount, format: .currency(code: "USD"))
+                    }
                 }
                 .onDelete(perform: removeItems)
             }
@@ -27,6 +37,7 @@ struct ContentView: View {
                     showingAddExpense = true
                 }
             }
+            
             .sheet(isPresented: $showingAddExpense){
                 AddView(expenses: expenses)
             }
